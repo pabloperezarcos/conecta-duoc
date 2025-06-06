e16cdb25d91a122f35ab91074bc46a1ee31dc299
pabloperezarcos <pabloperezarcos@gmail.com>
2025-06-05 22:50:25 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,52 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+
+// MSAL
+import { MsalModule } from '@azure/msal-angular';
+import { PublicClientApplication, InteractionType } from '@azure/msal-browser';
+
+
+(async () => {
+  // 1) Instancia Msal
+  const pca = new PublicClientApplication({
+    auth: {
+      clientId: '',
+      authority: 'https://login.microsoftonline.com/',
+      redirectUri: '',
+    },
+    // system: { asyncJIT: true }  // normalmente ya viene true por defecto en versiones nuevas
+  });
+
+  // 2) Esperar inicialización
+  await pca.initialize();
+
+  // 3) Importar MsalModule
+  bootstrapApplication(AppComponent, {
+    providers: [
+      ...appConfig.providers,
+      provideHttpClient(),
+      importProvidersFrom(
+        BrowserModule,
+        MsalModule.forRoot(
+          pca,
+          {
+            interactionType: InteractionType.Popup,
+            authRequest: { scopes: ['user.read'] },
+          },
+          {
+            interactionType: InteractionType.Redirect,
+            protectedResourceMap: new Map([
+              ['https://graph.microsoft.com/v1.0/me', ['user.read']],
+            ]),
+          }
+        )
+      ),
+    ]
+  })
+    .catch(err => console.error(err));
+})();
