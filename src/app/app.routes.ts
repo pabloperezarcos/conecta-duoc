be5636c545894bb4093479b6d20518a476efdbac
pabloperezarcos <pabloperezarcos@gmail.com>
2025-06-05 22:50:19 -0400

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent),
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent),
+        canActivate: [AuthGuard]
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
