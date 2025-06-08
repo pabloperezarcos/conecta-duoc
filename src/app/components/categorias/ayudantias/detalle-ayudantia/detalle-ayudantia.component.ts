488b8bcef90d6411b72873c28d4b3dda50788085
pabloperezarcos <pabloperezarcos@gmail.com>
2025-06-08 01:28:01 -0400

    Update Detalle Ayudantia

--- src/app/components/categorias/ayudantias/detalle-ayudantia/detalle-ayudantia.component.ts
+++ src/app/components/categorias/ayudantias/detalle-ayudantia/detalle-ayudantia.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from '../../../navbar/navbar.component';
 import { BreadcrumbComponent } from '../../../breadcrumb/breadcrumb.component';
 import { PublicacionesService } from '../../../../core/services/publicaciones.service';
 import { UserService } from '../../../../core/services/user.service';
@@ -11,7 +10,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 @Component({
   selector: 'app-detalle-ayudantia',
   standalone: true,
-  imports: [CommonModule, RouterModule, NavbarComponent, BreadcrumbComponent, ReactiveFormsModule],
+  imports: [CommonModule, RouterModule, BreadcrumbComponent, ReactiveFormsModule],
   templateUrl: './detalle-ayudantia.component.html',
   styleUrls: ['./detalle-ayudantia.component.scss']
 })
