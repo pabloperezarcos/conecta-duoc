f53a290d6a78d2fe967bc13525ca84c994660163
pabloperezarcos <pabloperezarcos@gmail.com>
2025-06-08 01:27:19 -0400

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,48 +6,57 @@ export const routes: Routes = [
     {
         path: '',
         loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent),
+        data: { showNavbar: false, showFooter: false }
     },
     {
         path: 'dashboard/perfil',
         loadComponent: () =>
             import('./components/perfil/perfil.component').then(m => m.PerfilComponent),
         canActivate: [AuthGuard],
+        data: { showNavbar: true, showFooter: true }
         //data: { allowedRoles: ['alumno', 'admin'] }
     },
     {
         path: 'dashboard',
         loadComponent: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent),
-        canActivate: [AuthGuard]
+        canActivate: [AuthGuard],
+        data: { showNavbar: true, showFooter: true }
     },
     {
         path: 'categoria/ayudantias',
         loadComponent: () => import('./components/categorias/ayudantias/ayudantias.component').then(m => m.AyudantiasComponent),
-        canActivate: [AuthGuard]
+        canActivate: [AuthGuard],
+        data: { showNavbar: true, showFooter: true }
     },
     {
         path: 'dashboard/ayudantias/:id',
         loadComponent: () => import('./components/categorias/ayudantias/detalle-ayudantia/detalle-ayudantia.component').then(m => m.DetalleAyudantiaComponent),
-        canActivate: [AuthGuard]
+        canActivate: [AuthGuard],
+        data: { showNavbar: true, showFooter: true }
     },
     {
         path: 'categoria/culturales',
         loadComponent: () => import('./components/categorias/culturales/culturales.component').then(m => m.CulturalesComponent),
-        canActivate: [AuthGuard]
+        canActivate: [AuthGuard],
+        data: { showNavbar: true, showFooter: true }
     },
     {
         path: 'categoria/deportes',
         loadComponent: () => import('./components/categorias/deportes/deportes.component').then(m => m.DeportesComponent),
-        canActivate: [AuthGuard]
+        canActivate: [AuthGuard],
+        data: { showNavbar: true, showFooter: true }
     },
     {
         path: 'categoria/trueques',
         loadComponent: () => import('./components/categorias/deportes/deportes.component').then(m => m.DeportesComponent),
-        canActivate: [AuthGuard]
+        canActivate: [AuthGuard],
+        data: { showNavbar: true, showFooter: true }
     },
     {
         path: 'categoria/voluntariado',
         loadComponent: () => import('./components/categorias/voluntariado/voluntariado.component').then(m => m.VoluntariadoComponent),
-        canActivate: [AuthGuard]
+        canActivate: [AuthGuard],
+        data: { showNavbar: true, showFooter: true }
     },
     {
         path: '**',
