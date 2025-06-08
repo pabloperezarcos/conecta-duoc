bade763ec4aecd2718f383fd154c74ab8aab166d
pabloperezarcos <pabloperezarcos@gmail.com>
2025-06-07 23:30:12 -0400

    Update Ayudantia

--- src/app/components/categorias/ayudantias/ayudantias.component.ts
+++ src/app/components/categorias/ayudantias/ayudantias.component.ts
@@ -7,11 +7,13 @@ import { PublicacionesService } from '../../../core/services/publicaciones.servi
 import { UserService } from '../../../core/services/user.service';
 import { Publicacion } from '../../../models/publicacion';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-ayudantias',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, BreadcrumbComponent, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, NavbarComponent, BreadcrumbComponent, ReactiveFormsModule, RouterLink, FormsModule],
   templateUrl: './ayudantias.component.html',
   styleUrls: ['./ayudantias.component.scss']
 })
@@ -21,6 +23,8 @@ export class AyudantiasComponent implements OnInit {
   publicaciones: Publicacion[] = [];
   publicacionEditando: Publicacion | null = null;
   comentarioForms: { [pubId: number]: FormGroup } = {};
+  filtroBusqueda: string = '';
+
 
   constructor(
     private fb: FormBuilder,
@@ -104,5 +108,17 @@ export class AyudantiasComponent implements OnInit {
     pub.comentarios.push(nuevoComentario);
     form.reset();
   }
+
+  get publicacionesFiltradas(): Publicacion[] {
+    if (!this.filtroBusqueda.trim()) return this.publicaciones;
+
+    const texto = this.filtroBusqueda.trim().toLowerCase();
+
+    return this.publicaciones.filter(pub =>
+      pub.titulo.toLowerCase().includes(texto) ||
+      pub.descripcion.toLowerCase().includes(texto)
+    );
+  }
+
 
 }
