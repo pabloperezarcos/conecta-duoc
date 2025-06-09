3e453cfb614728c7b71abb80202be6d255c98ddc
pabloperezarcos <pabloperezarcos@gmail.com>
2025-06-08 20:10:39 -0400

    Update reglas-de-la-comunidad.component.ts

--- src/app/components/reglas-de-la-comunidad/reglas-de-la-comunidad.component.ts
+++ src/app/components/reglas-de-la-comunidad/reglas-de-la-comunidad.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { ModalConfirmacionComponent } from '../shared/modal-confirmacion/modal-confirmacion.component';
+import { ModalConfirmacionComponent } from '../../shared/modal-confirmacion/modal-confirmacion.component';
 
 @Component({
   selector: 'app-reglas-de-la-comunidad',
