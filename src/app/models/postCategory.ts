8e8cab53fc289fe3a4d5e7560651fa798fc0d959
pabloperezarcos <pabloperezarcos@gmail.com>
2025-06-22 20:56:26 -0400

    Update postCategory.ts

--- src/app/models/postCategory.ts
+++ src/app/models/postCategory.ts
@@ -2,5 +2,5 @@ export interface PostCategory {
     idCategory: number;
     categoryName: string;
     description: string;
-    status: boolean; // 0 Inactivo, 1 Activo
+    status: number; // 0 Inactivo, 1 Activo
 }
